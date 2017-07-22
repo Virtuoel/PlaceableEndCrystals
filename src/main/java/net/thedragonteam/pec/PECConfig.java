1a539fa2b6829d788da18331648c0f7790575a44
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2017-07-22 11:01:26 +0300

    added debugmode in config

--- src/main/java/net/thedragonteam/pec/PECConfig.java
+++ src/main/java/net/thedragonteam/pec/PECConfig.java
@@ -6,4 +6,7 @@ public class PECConfig {
 
     @ModConfigProperty(category = "end_crystal", name = "hasEnderCrystalBasePlate", comment = "Enable/Disable the base from the Ender Crystal")
     public static boolean hasEnderCrystalBasePlate = false;
+
+    @ModConfigProperty(category = "end_crystal", name = "debugMode", comment = "Enable/Disable debugMode")
+    public static boolean debugMode = false;
 }
